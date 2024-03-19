482ae54a56083fd53de308ccffef7fd064caaf58
wagnerlm <wagnerlink@gmail.com>
2024-03-19 14:46:46 -0300

    fix cognito

--- cognito.tf
+++ cognito.tf
@@ -4,36 +4,52 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket         = "point-terraform-state"
-    key            = "point-cognito.tfstate"
-    region         = "us-east-2"
-    encrypt        = true
+    bucket  = "point-terraform-state"
+    key     = "point-cognito.tfstate"
+    region  = "us-east-2"
+    encrypt = true
   }
+}
+
+data "aws_lambda_function" "point_lambda_pre_sign_up" {
+  function_name = "point_lambda_pre_sign_up"
 }
 
 resource "aws_cognito_user_pool" "cognito_user_pool" {
   name = "point"
+
   lambda_config {
-    pre_sign_up = "arn:aws:lambda:us-east-2:644237782704:function:point_lambda_pre_sign_up"
+    pre_sign_up = data.aws_lambda_function.point_lambda_pre_sign_up.arn
   }
+
   password_policy {
-    minimum_length = 6
+    minimum_length    = 6
     require_lowercase = false
-    require_numbers = false
-    require_symbols = false
+    require_numbers   = false
+    require_symbols   = false
     require_uppercase = false
   }
   mfa_configuration = "OFF"
 }
 
-resource "aws_cognito_user_pool_client" "point-app-client" {
-  name = "point-cognito-app-client"
-  user_pool_id = aws_cognito_user_pool.cognito_user_pool.id
-  allowed_oauth_flows = ["code", "implicit"]
+resource "aws_cognito_user_pool_client" "point_app_client" {
+  name                                 = "point_app_client"
+  user_pool_id                         = aws_cognito_user_pool.cognito_user_pool.id
+  allowed_oauth_flows                  = ["code", "implicit"]
   allowed_oauth_flows_user_pool_client = true
-  allowed_oauth_scopes = ["openid", "profile"]
-  callback_urls = ["https://example.com/callback"]
-  logout_urls = ["https://example.com/logout"]
-  supported_identity_providers = ["COGNITO"]
-  explicit_auth_flows = ["ALLOW_ADMIN_USER_PASSWORD_AUTH", "ALLOW_CUSTOM_AUTH", "ALLOW_REFRESH_TOKEN_AUTH", "ALLOW_USER_PASSWORD_AUTH", "ALLOW_USER_SRP_AUTH"]
+  allowed_oauth_scopes                 = ["openid", "profile"]
+  callback_urls                        = ["https://example.com/callback"]
+  logout_urls                          = ["https://example.com/logout"]
+  supported_identity_providers         = ["COGNITO"]
+  explicit_auth_flows                  = [
+    "ALLOW_ADMIN_USER_PASSWORD_AUTH", "ALLOW_CUSTOM_AUTH", "ALLOW_REFRESH_TOKEN_AUTH", "ALLOW_USER_PASSWORD_AUTH",
+    "ALLOW_USER_SRP_AUTH"
+  ]
+}
+
+resource "aws_lambda_permission" "point_lambda_pre_sign_up_permission" {
+  principal     = "cognito-idp.amazonaws.com"
+  action        = "lambda:InvokeFunction"
+  function_name = data.aws_lambda_function.point_lambda_pre_sign_up.function_name
+  source_arn    = aws_cognito_user_pool.cognito_user_pool.arn
 }
